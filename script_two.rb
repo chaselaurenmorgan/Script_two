1ec64e458b70d7e1845f6cd5c3d2e58fff4ab837
Chase Morgan <chasemorgan15@yahoo.com>
2017-10-21 22:25:59 -0700

    Changed the file read

--- script_two.rb
+++ script_two.rb
@@ -1,15 +1,18 @@
 require 'selenium-webdriver'
 require 'gmail'
 
-file = File.open("urls.txt", 'r')
-while !file.eof?
-  line = file.readline.chomp
-  urls = [line]
+@urls = []
+infile = File.open('urls.txt', 'r')
+while (line =infile.gets)
+@urls << line.to_s.chomp
+end
+
 
 driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "https://www.webpagetest.org/"
-driver.find_element(:id,'url').send_keys("#{urls}")
-elements = driver.find_element(:id, 'star_test-container').click
+driver.find_element(:id, "url").send_keys("#{@urls}")
+driver.find_element(:class, "start_test").click
+
 
 wait = Selenium::WebDriver::Wait.new(:timeout => 60)
 wait.until { driver.find_element(:id => "LoadTime") }
@@ -19,17 +22,18 @@ wait.until { driver.find_element(:id => "TTFB") }
   driver.find_element(:id, 'TTFB').text
 
   puts driver.find_element(:id, "LoadTime").text
+  puts driver.find_element(:id, 'TTFB').text
 
   load_time = driver.find_element(:id, "LoadTime").text
   time_to_first_byte = driver.find_element(:id, "TTFB").text
-end
+
 
   gmail = Gmail.connect('username', 'password')
   email = gmail.compose do
-    to "chasemorgan15@yahoo.com"
+    to "reciver@yahoo.com"
     subject "First Testing Script"
     body "Values are #{time_to_first_byte}(TTFB) and #{load_time}(LoadTime)
-    from the url https://www.ebay.com"
+    from the url #{@urls}"
 end
 email.deliver!
 sleep 50
