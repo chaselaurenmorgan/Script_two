83495c80a8f9a3ff7546db815dc152c293d2f0d2
Chase Morgan <chasemorgan15@yahoo.com>
2017-11-02 14:33:46 -0700

    Second script modifications

--- original.rb
+++ original.rb
@@ -1,19 +1,28 @@
 require 'selenium-webdriver'
 require 'gmail'
 
-@urls = []
 
-  File.open("urls.txt").each do |line|
-  line = line.strip
-  @urls << line.strip
+@urls = []
+File.each("urls.txt").with_index do |line, line_num|
+
+  @urls = line.to_s.chomp
+  @line_num = line_num.to_i
+  begin @urls
+   puts "#{@line_num}: #{@urls}"
+
+end
 
 driver = Selenium::WebDriver.for :chrome
+@email_body = " "
+
+
+
 driver.navigate.to "https://www.webpagetest.org/"
-driver.find_element(:id, "url").send_keys("#{line}")
+driver.find_element(:id, "url").send_keys("#{@urls}")
 driver.find_element(:class, "start_test").click
 
 
-wait = Selenium::WebDriver::Wait.new(:timeout => 60)
+wait = Selenium::WebDriver::Wait.new(:timeout => 80)
 wait.until { driver.find_element(:id => "LoadTime") }
 wait.until { driver.find_element(:id => "TTFB") }
 
@@ -23,17 +32,33 @@ wait.until { driver.find_element(:id => "TTFB") }
   puts driver.find_element(:id, "LoadTime").text
   puts driver.find_element(:id, 'TTFB').text
 
-  load_time = driver.find_element(:id, "LoadTime").text
-  time_to_first_byte = driver.find_element(:id, "TTFB").text
+  @load_time = driver.find_element(:id, "LoadTime").text
+  @time_to_first_byte = driver.find_element(:id, "TTFB").text
 
-  gmail = Gmail.connect('username', 'password')
-  email = gmail.compose do
-    to "@gmail.com"
-    subject "First Testing Script"
-    body "Values are #{time_to_first_byte}(TTFB) and #{load_time}(LoadTime)
-    from the url #{@urls}"
+@email_body = "Values are #{@time_to_first_byte}(TTFB) and #{@load_time}(LoadTime)
+from the #{@urls}"
 
+puts @email_body += 1.to_s
+
+driver.quit
 end
-email.deliver!
+
+@email_body = "Values are #{@time_to_first_byte}(TTFB) and #{@load_time}(LoadTime)
+from the #{@urls}"
+
+@gmail = Gmail.connect('username', 'password')
+  email = @gmail.compose do
+    to 'username@yahoo.com'
+  @line_num = 1
+    if @line_num == 1
+     subject 'Second Ruby Script!'
+      body "Here is your data:" +@email_body
+      puts ' email has been sent'
+    else
+      break
+      end
+  end
+  email.deliver!
+
+
 sleep 50
-end
---
+++ script_two.rb
@@ -0,0 +1,38 @@
+require 'selenium-webdriver'
+require 'gmail'
+
+ # @urls = "test_url"
+ # @urls << test_url.to_s.chomp
+
+File.foreach('urls.txt') do |test_url|
+puts "#{test_url}"
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "https://www.webpagetest.org/"
+driver.find_element(:id, "url").send_keys("#{@urlsl}")
+driver.find_element(:class, "start_test").click
+
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 60)
+wait.until { driver.find_element(:id => "LoadTime") }
+
+  driver.find_element(:id, 'LoadTime').text
+  driver.find_element(:id, 'TTFB').text
+
+  puts driver.find_element(:id, "LoadTime").text
+  puts driver.find_element(:id, 'TTFB').text
+
+  load_time = driver.find_element(:id, "LoadTime").text
+  time_to_first_byte = driver.find_element(:id, "TTFB").text
+  x += 1
+
+end
+
+  gmail = Gmail.connect('usernams', 'password')
+  email = gmail.compose do
+    to "@gmail.com"
+    subject "First Testing Script"
+    body "Values are #{time_to_first_byte}(TTFB) and #{load_time}(LoadTime)
+    from the #{@urls}"
+end
+email.deliver!
+sleep 5
