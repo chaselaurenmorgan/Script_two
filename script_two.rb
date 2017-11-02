80620d5f56cc640316326acdc282a7c9db99d37f
Chase Morgan <chasemorgan15@yahoo.com>
2017-11-02 14:37:11 -0700

    Commented out

--- script_two.rb
+++ script_two.rb
@@ -1,38 +1,37 @@
-require 'selenium-webdriver'
-require 'gmail'
+#require 'selenium-webdriver'
+#require 'gmail'
 
  # @urls = "test_url"
  # @urls << test_url.to_s.chomp
 
-File.foreach('urls.txt') do |test_url|
-puts "#{test_url}"
-driver = Selenium::WebDriver.for :chrome
-driver.navigate.to "https://www.webpagetest.org/"
-driver.find_element(:id, "url").send_keys("#{@urlsl}")
-driver.find_element(:class, "start_test").click
+#puts "#{test_url}"
+#driver = Selenium::WebDriver.for :chrome
+#driver.navigate.to "https://www.webpagetest.org/"
+#driver.find_element(:id, "url").send_keys("#{@urlsl}")
+#driver.find_element(:class, "start_test").click
 
 
-wait = Selenium::WebDriver::Wait.new(:timeout => 60)
-wait.until { driver.find_element(:id => "LoadTime") }
+#wait = Selenium::WebDriver::Wait.new(:timeout => 60)
+#wait.until { driver.find_element(:id => "LoadTime") }
 
-  driver.find_element(:id, 'LoadTime').text
-  driver.find_element(:id, 'TTFB').text
+#  driver.find_element(:id, 'LoadTime').text
+#  driver.find_element(:id, 'TTFB').text
 
-  puts driver.find_element(:id, "LoadTime").text
-  puts driver.find_element(:id, 'TTFB').text
+#  puts driver.find_element(:id, "LoadTime").text
+#  puts driver.find_element(:id, 'TTFB').text
 
-  load_time = driver.find_element(:id, "LoadTime").text
-  time_to_first_byte = driver.find_element(:id, "TTFB").text
-  x += 1
+#  load_time = driver.find_element(:id, "LoadTime").text
+#  time_to_first_byte = driver.find_element(:id, "TTFB").text
+#  x += 1
 
-end
+#end
 
-  gmail = Gmail.connect('usernams', 'password')
-  email = gmail.compose do
-    to "@gmail.com"
-    subject "First Testing Script"
-    body "Values are #{time_to_first_byte}(TTFB) and #{load_time}(LoadTime)
-    from the #{@urls}"
-end
-email.deliver!
-sleep 5
+#  gmail = Gmail.connect('username', 'password')
+#  email = gmail.compose do
+#    to "@gmail.com"
+#    subject "First Testing Script"
+#    body "Values are #{time_to_first_byte}(TTFB) and #{load_time}(LoadTime)
+#    from the #{@urls}"
+#end
+#email.deliver!
+#sleep 5
